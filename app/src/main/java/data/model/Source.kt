ca821e16048249a89d8fb7f89c26e1f9b4822a97
Joe Mobarak <mobarak.joe@gmail.com>
2019-09-20 11:24:46 +0300

    Create source model using kotlin to json plugin with gson annotation

---
+++ app/src/main/java/data/model/Source.kt
@@ -0,0 +1,10 @@
+package data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Source(
+    @SerializedName("id")
+    val id: Any,
+    @SerializedName("name")
+    val name: String
+)
