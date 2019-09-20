fc9da359f47ff25d4ff06518346f0e387139a751
Joe Mobarak <mobarak.joe@gmail.com>
2019-09-20 10:59:50 +0300

    Refactor main acitivity to news activity

---
+++ app/src/main/java/com/technology/joe/inmobileschallenge/ui/NewsActivity.kt
@@ -0,0 +1,13 @@
+package com.technology.joe.inmobileschallenge.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.technology.joe.inmobileschallenge.R
+
+class NewsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+    }
+}
