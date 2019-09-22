b134c803cad0334a1f87bb063b286dfaef4ac75b
JoeMobarak <mobarak.joe@gmail.com>
2019-09-22 13:28:06 +0300

    Call get all news api fun in repository

--- app/src/main/java/data/repository/NewsRepository.kt
+++ app/src/main/java/data/repository/NewsRepository.kt
@@ -1,6 +1,24 @@
 package data.repository
 
+import data.model.Article
+import network.api.NewsAPI
+
 class NewsRepository {
 
+    fun getAll(
+        date: String,
+        type: String,
+        sortBy: String,
+        apiKey: String,
+        closure: (List<Article>) -> Unit
+    ) {
+        val newsApi = NewsAPI()
+        newsApi.getAll(date, type, sortBy, apiKey)
+        {
+            it?.let {
+                closure(it)
+            }
+        }
+    }
 
 }
