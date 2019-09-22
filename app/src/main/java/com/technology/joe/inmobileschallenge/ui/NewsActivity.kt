29ccd88ee6d18fdd92de388ffd50ffe7fd0e57f3
JoeMobarak <mobarak.joe@gmail.com>
2019-09-22 14:10:15 +0300

    Set up recycler view in news activity

--- app/src/main/java/com/technology/joe/inmobileschallenge/ui/NewsActivity.kt
+++ app/src/main/java/com/technology/joe/inmobileschallenge/ui/NewsActivity.kt
@@ -1,13 +1,27 @@
 package com.technology.joe.inmobileschallenge.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.technology.joe.inmobileschallenge.R
+import com.technology.joe.inmobileschallenge.ui.adapter.NewsAdapter
+import data.model.Article
+import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
+    private lateinit var adapter: NewsAdapter
+    private val storesList: ArrayList<Article> = ArrayList()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+        setUpRecyclerView()
+    }
+
+    private fun setUpRecyclerView() {
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        adapter = NewsAdapter(storesList)
+        recycler_view.adapter = adapter
     }
 }
