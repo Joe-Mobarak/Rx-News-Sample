fdd0b61c886766726df1e8c068bd6ce40eff5b31
JoeMobarak <mobarak.joe@gmail.com>
2019-09-22 13:27:29 +0300

    Develop news adapter

--- app/src/main/java/com/technology/joe/inmobileschallenge/ui/adapter/NewsAdapter.kt
+++ app/src/main/java/com/technology/joe/inmobileschallenge/ui/adapter/NewsAdapter.kt
@@ -1,6 +1,38 @@
 package com.technology.joe.inmobileschallenge.ui.adapter
 
-class NewsAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.technology.joe.inmobileschallenge.R
+import com.technology.joe.inmobileschallenge.databinding.NewsRowBinding
+import data.model.Article
 
+class NewsAdapter(private val news: ArrayList<Article>) :
+    RecyclerView.Adapter<NewsAdapter.NewsItemViewHolder>() {
+    override fun getItemCount(): Int {
+        return news.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsItemViewHolder {
+        return NewsItemViewHolder(
+            DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                R.layout.row_news,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: NewsItemViewHolder, position: Int) {
+        val model = news[position]
+        holder.binding.authorText.text = model.author
+        holder.binding.titleText.text = model.title
+    }
+
+
+    class NewsItemViewHolder(val binding: NewsRowBinding) :
+        RecyclerView.ViewHolder(binding.root)
 
 }
