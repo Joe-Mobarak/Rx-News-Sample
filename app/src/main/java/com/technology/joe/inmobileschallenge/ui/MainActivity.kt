a443e58ac03f60c3e03647022813705f28c84d98
JoeMobarak <mobarak.joe@gmail.com>
2019-09-22 19:54:21 +0300

    Create main activity and make it the main in manifest

---
+++ app/src/main/java/com/technology/joe/inmobileschallenge/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.technology.joe.inmobileschallenge.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.technology.joe.inmobileschallenge.R
+import data.model.Article
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
