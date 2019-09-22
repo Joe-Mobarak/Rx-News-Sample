c60de1eb8cdda58256b7134b39faee06d2fb58d9
JoeMobarak <mobarak.joe@gmail.com>
2019-09-22 11:42:27 +0300

    Provide an instance for newsviewmodel

--- app/src/main/java/com/technology/joe/inmobileschallenge/ui/viewmodel/NewsViewModelFactory.kt
+++ app/src/main/java/com/technology/joe/inmobileschallenge/ui/viewmodel/NewsViewModelFactory.kt
@@ -1,8 +1,13 @@
 package com.technology.joe.inmobileschallenge.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 class NewsViewModelFactory() :
     ViewModelProvider.NewInstanceFactory() {
 
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return NewsViewModel() as T
+    }
 }
