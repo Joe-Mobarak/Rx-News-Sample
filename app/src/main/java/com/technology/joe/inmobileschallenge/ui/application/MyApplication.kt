4e220a532819526513768e32f785d2060c302f12
JoeMobarak <mobarak.joe@gmail.com>
2019-09-22 23:42:36 +0300

    Apply kodein depedency injection

--- app/src/main/java/com/technology/joe/inmobileschallenge/ui/application/MyApplication.kt
+++ app/src/main/java/com/technology/joe/inmobileschallenge/ui/application/MyApplication.kt
@@ -1,7 +1,26 @@
 package com.technology.joe.inmobileschallenge.ui.application
 
 import android.app.Application
+import com.technology.joe.inmobileschallenge.ui.viewmodel.NewsViewModelFactory
+import data.repository.NewsRepository
+import network.api.NewsAPI
+import network.builder.RetrofitBuilder
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
 
-class MyApplication : Application() {
+class MyApplication : Application(), KodeinAware {
+
+    //Setup Dependency Injection
+    override val kodein = Kodein.lazy {
+        import(androidXModule(this@MyApplication))
+        bind() from provider { NewsViewModelFactory(instance()) }
+        bind() from provider { NewsRepository(instance()) }
+        bind() from provider { NewsAPI(instance()) }
+        bind() from provider { RetrofitBuilder() }
+    }
 
 }
