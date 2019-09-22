c2165442ecbd06f1df023a2b858d72422d47d679
JoeMobarak <mobarak.joe@gmail.com>
2019-09-22 19:56:05 +0300

    Develop add fragment fun and add the news list fragment on launch

--- app/src/main/java/com/technology/joe/inmobileschallenge/ui/MainActivity.kt
+++ app/src/main/java/com/technology/joe/inmobileschallenge/ui/MainActivity.kt
@@ -7,9 +7,38 @@ import data.model.Article
 
 class MainActivity : AppCompatActivity() {
 
+    enum class FragmentTypes {
+        NEWS,
+        NEWS_DETAILS
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addFragment(FragmentTypes.NEWS, null)
+
     }
 
+    fun addFragment(type: FragmentTypes, article: Article?) {
+        if (type == FragmentTypes.NEWS) {
+            supportFragmentManager.beginTransaction()
+                .replace(
+                    R.id.container,
+                    NewsFragment()
+                )
+                .addToBackStack("ss")
+                .commit()
+        } else {
+            article?.let {
+                supportFragmentManager.beginTransaction()
+                    .replace(
+                        R.id.container,
+                        NewsDetailsFragment.newInstance(it)
+                    )
+                    .addToBackStack("ss")
+                    .commit()
+
+            }
+        }
+    }
 }
