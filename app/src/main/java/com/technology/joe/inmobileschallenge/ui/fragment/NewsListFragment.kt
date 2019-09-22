e5312655fbaa68b24bf6a0aef353dd443032ef47
JoeMobarak <mobarak.joe@gmail.com>
2019-09-22 23:42:12 +0300

    Make fragment kodein aware and instantiate viewmodel from factory

--- app/src/main/java/com/technology/joe/inmobileschallenge/ui/fragment/NewsListFragment.kt
+++ app/src/main/java/com/technology/joe/inmobileschallenge/ui/fragment/NewsListFragment.kt
@@ -6,19 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import base.Resource
 import com.technology.joe.inmobileschallenge.R
 import com.technology.joe.inmobileschallenge.ui.activity.NewsActivity
 import com.technology.joe.inmobileschallenge.ui.adapter.NewsAdapter
 import com.technology.joe.inmobileschallenge.ui.viewmodel.NewsViewModel
+import com.technology.joe.inmobileschallenge.ui.viewmodel.NewsViewModelFactory
 import data.model.Article
 import kotlinx.android.synthetic.main.fragment_news_list.*
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.closestKodein
+import org.kodein.di.generic.instance
 
-class NewsListFragment : Fragment() {
+class NewsListFragment : Fragment(), KodeinAware {
     private lateinit var adapter: NewsAdapter
     private lateinit var viewModel: NewsViewModel
     private val storesList: ArrayList<Article> = ArrayList()
+    override val kodein by closestKodein()
+    private val viewModelFactory: NewsViewModelFactory by instance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -50,7 +57,8 @@ class NewsListFragment : Fragment() {
     }
 
     private fun setUpViewModel() {
-        viewModel = NewsViewModel()
+        viewModel =
+            ViewModelProviders.of(this, viewModelFactory).get(NewsViewModel::class.java)
         viewModel.news.observe(this, Observer {
             it?.let {
                 if (it.status == Resource.STATUS_NO_INTERNET) {
