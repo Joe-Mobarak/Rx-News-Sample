9c543bf95eacf013008a61ba3a54e0808a84a4ba
JoeMobarak <mobarak.joe@gmail.com>
2019-09-22 19:47:43 +0300

    Make news activity a fragment

--- app/src/main/java/com/technology/joe/inmobileschallenge/ui/NewsFragment.kt
+++ app/src/main/java/com/technology/joe/inmobileschallenge/ui/NewsFragment.kt
@@ -1,8 +1,10 @@
 package com.technology.joe.inmobileschallenge.ui
 
 import android.os.Bundle
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.technology.joe.inmobileschallenge.R
@@ -11,22 +13,28 @@ import com.technology.joe.inmobileschallenge.ui.viewmodel.NewsViewModel
 import data.model.Article
 import kotlinx.android.synthetic.main.activity_news.*
 
-class NewsActivity : AppCompatActivity() {
+class NewsFragment : Fragment() {
     private lateinit var adapter: NewsAdapter
     private lateinit var viewModel: NewsViewModel
     private val storesList: ArrayList<Article> = ArrayList()
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.activity_news, container, false)
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_news)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         setUpRecyclerView()
         setUpViewModel()
         setPullToRefresh()
     }
 
+
     private fun setUpRecyclerView() {
-        recycler_view.layoutManager = LinearLayoutManager(this)
+        recycler_view.layoutManager = LinearLayoutManager(activity)
         adapter = NewsAdapter(storesList, object : NewsAdapter.INewsItemSelected {
             override fun onNewsItemsClicked(position: Int) {
             }
