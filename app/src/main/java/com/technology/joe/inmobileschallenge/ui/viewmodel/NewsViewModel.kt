b0ba92df83aa96c63614518783a079f3fcfaa1d3
JoeMobarak <mobarak.joe@gmail.com>
2019-09-22 13:30:14 +0300

    Add articles list as livedata to be later observed by our ui

--- app/src/main/java/com/technology/joe/inmobileschallenge/ui/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/technology/joe/inmobileschallenge/ui/viewmodel/NewsViewModel.kt
@@ -1,8 +1,13 @@
 package com.technology.joe.inmobileschallenge.ui.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import data.model.Article
 
-class NewsViewModel() : ViewModel() {
-
+class NewsViewModel : ViewModel() {
+    private val _news: MutableLiveData<List<Article>> = MutableLiveData()
+    val news: LiveData<List<Article>>
+        get() = _news
 
 }
