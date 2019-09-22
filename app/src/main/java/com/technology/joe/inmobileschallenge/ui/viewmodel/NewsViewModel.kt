1f5eea9e66b7669d0126e503ef664938176ba9d0
JoeMobarak <mobarak.joe@gmail.com>
2019-09-23 00:20:43 +0300

    Load apikey and baseurl from build config

--- app/src/main/java/com/technology/joe/inmobileschallenge/ui/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/technology/joe/inmobileschallenge/ui/viewmodel/NewsViewModel.kt
@@ -4,11 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import base.Resource
+import com.technology.joe.inmobileschallenge.BuildConfig
 import data.model.Article
 import data.repository.NewsRepository
 import util.Utils
-import java.text.SimpleDateFormat
-import java.util.*
 
 class NewsViewModel(private val newsRepository: NewsRepository) : ViewModel() {
     private val _news: MutableLiveData<Resource<List<Article>>> = MutableLiveData()
@@ -19,11 +18,10 @@ class NewsViewModel(private val newsRepository: NewsRepository) : ViewModel() {
         getAllNews()
     }
 
-     fun getAllNews() {
+    fun getAllNews() {
         val type = "news"
         val sortBy = "publishedAt"
-        val apiKey = "c7a80f45eac941299d1e96acdf742568"
-        newsRepository.getAll(Utils.getCurrentDate(), type, sortBy, apiKey) {
+        newsRepository.getAll(Utils.getCurrentDate(), type, sortBy, BuildConfig.apiKey) {
             _news.postValue(it)
         }
 
