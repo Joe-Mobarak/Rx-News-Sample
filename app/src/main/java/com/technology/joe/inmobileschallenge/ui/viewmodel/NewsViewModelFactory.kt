aca973cd0150548e9b6b4f0c9aab8ff8bb646d47
JoeMobarak <mobarak.joe@gmail.com>
2019-09-22 11:38:38 +0300

    Create a newsviewmodel factory class

---
+++ app/src/main/java/com/technology/joe/inmobileschallenge/ui/viewmodel/NewsViewModelFactory.kt
@@ -0,0 +1,8 @@
+package com.technology.joe.inmobileschallenge.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+
+class NewsViewModelFactory() :
+    ViewModelProvider.NewInstanceFactory() {
+
+}
