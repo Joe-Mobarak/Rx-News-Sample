01dc72503a06e80fa5cc1947ba36487e194d9ecc
JoeMobarak <mobarak.joe@gmail.com>
2019-09-23 01:13:20 +0300

    Develop convert iso date fun

--- app/src/main/java/util/Utils.kt
+++ app/src/main/java/util/Utils.kt
@@ -1,5 +1,6 @@
 package util
 
+import info.vividcode.time.iso8601.Iso8601ExtendedOffsetDateTimeFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -10,6 +11,18 @@ class Utils {
         fun getCurrentDate(): String {
             val sdf = SimpleDateFormat(("yyyy/MM/dd"), Locale.US)
             return sdf.format(Calendar.getInstance().time)
+        }
+
+        fun formatISODate(isoDate: String): Pair<String, String>? {
+            val isoFormatter = Iso8601ExtendedOffsetDateTimeFormat()
+            val date = isoFormatter.parse(isoDate)
+            val dayFormat = SimpleDateFormat(("dd-MM-yyyy"), Locale.US)
+            val hourFormat = SimpleDateFormat(("hh:mm a"), Locale.US)
+            date?.let {
+                return Pair(dayFormat.format(it), hourFormat.format(it))
+            } ?: kotlin.run {
+                return null
+            }
         }
     }
 }
