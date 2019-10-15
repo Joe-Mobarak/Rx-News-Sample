99f7a23ab4e15cbf34166fb4a917887ff25ba4e6
JoeMobarak <mobarak.joe@gmail.com>
2019-10-15 03:46:45 -0700

    Add url builder class where application urls should be stored

---
+++ app/src/main/java/network/builder/UrlBuilder.kt
@@ -0,0 +1,12 @@
+package network.builder
+
+import com.technology.joe.inmobileschallenge.BuildConfig
+
+object UrlBuilder {
+
+    const val BASE_URL = BuildConfig.baseUrl
+
+    //news
+    const val GET_ALL = "/v2/everything"
+
+}
